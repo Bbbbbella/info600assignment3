caf1930e7f0f9eed66f29a6fcc7366bb0453a615
Bella Guo <bella@n3-47-75.dhcp.drexel.edu>
2019-11-19 15:52:48 -0500

    Hello again

--- js/main.js
+++ js/main.js
@@ -22,10 +22,56 @@ function assignClickHandler () {
     newChild.appendChild(document.createTextNode(newEntry))
 
     enteredRecords.appendChild(newChild)
+    var model = {
+      "fullName": $("#fullName").val(),
+      "major": $("#major").val(),
+      "startYear": Number($("#startYear").val())
+  };
+  add(model);
+   
+  })
+}
+$(function(){
+  search();
+})
 
-    document.getElementById('inputs').reset()
+function search() {
+  $.get("http://localhost:8081/data/entries.json", {}, function (data) {
+      if (data.records && data.records.length > 0) {
+          var htmlStr = "";
+          var date = new Date()
+          var hours = date.getHours().toString().padStart(2, '0')
+          var minutes = date.getMinutes().toString().padStart(2, '0')
+          var time = hours + ':' + minutes
+          for (var i = 0; i < data.records.length; i++) {
+              var deleteStr = "<button onclick='remove(\"" + data.records[i].id +"\")'>Delete</button>"
+              htmlStr = htmlStr + "<li>" + time + ' - ' + data.records[i].fullName + ", " + data.records[i].major + ", " + data.records[i].startYear + ", " + deleteStr + "</li>"
+          }
+          $("#enteredRecords").html(htmlStr);
+      }
+  });
+}
+
+function add(inputData) {
+  $.post("http://localhost:8081/user", inputData,function() {
+      if (data) {
+          alert("Add successfully"); 
+          document.getElementById('inputs').reset()
+          search();
+      } else {
+          alert("Fail to Add");
+      }
   })
 }
 
+function remove(id) {
+  $.get("http://localhost:8081/user/" + id, {}, function (data) {
+      if (data) {
+          alert("Delete Successfully");
+      } else {
+          alert("Fail to Delete");
+      }
+  })
+}
 
 
